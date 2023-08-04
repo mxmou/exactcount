0c83af5226019b1fa676ef8563e477c7f423d4e5
Maximouse <51849865+mxmou@users.noreply.github.com>
2023-08-04 17:27:04 +0200

    Include studio ID in URL (fixes #6)

--- exactcount.js
+++ exactcount.js
@@ -1,6 +1,8 @@
 const api = "https://mxmou.eu.pythonanywhere.com/api/exactcount";
 const pageSize = 40;
 
+let currentStudio = "";
+
 async function getPageLength(url, page, cache) {
 	if (Object.hasOwnProperty.call(cache, page)) return cache[page];
 	const res = await fetch(`${url}?limit=${pageSize}&offset=${page*pageSize}`);
@@ -40,6 +42,8 @@ function go(studio) {
 	} else {
 		document.querySelector("#studio-input").value = `https://scratch.mit.edu/studios/${studio}`;
 	}
+	currentStudio = studio;
+	location.hash = `#${studio}`;
 	const apiUrlPrefix = `${api}/studios/${studio}/projects`;
 	document.body.className = "waiting";
 	countProjects(apiUrlPrefix).then((count) => {
@@ -54,6 +58,21 @@ document.querySelector("#ok").addEventListener("click", () => go());
 document.querySelector("#studio-input").addEventListener("keyup", function(event) {
 	if (event.key == "Enter") go();
 })
+
+function handleHash() {
+	const studio = location.hash.slice(1);
+	// Don't do anything if the hash change was caused by clicking "Go"
+	if (studio == currentStudio) return;
+	if (!studio) {
+		document.querySelector("#studio-input").value = "";
+		document.body.className = ""; // Hide result from previous studio
+		currentStudio = "";
+		return;
+	}
+	go(studio);
+}
+if (location.hash) handleHash();
+window.addEventListener("hashchange", () => handleHash());
 
 fetch(`${api}/proxy/featured`).then(async (res) => {
 	document.querySelector("#examples").classList.remove("loading");
